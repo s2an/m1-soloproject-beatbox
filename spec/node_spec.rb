6d734a48bc60c40bac2afa0cb3e951188724d83e
Stephen Nash <stephen.aa.nash@gmail.com>
2023-10-13 15:55:48 -0400

    Create node_spec without failures

---
+++ spec/node_spec.rb
@@ -0,0 +1,27 @@
+require 'pry'
+require './lib/node'
+
+# node = Node.new("plop")
+# node.data
+# node.next_node
+
+RSpec.describe Node do
+  it 'exists' do
+    node = Node.new("plop")
+    
+    expect(node).to be_instance_of(Node)
+  end
+
+  it 'has data' do
+    node = Node.new("plop")
+
+    expect(node.data).to eq("plop")
+  end
+
+  it 'has data' do
+    node = Node.new("plop")
+
+    expect(node.next_node).to eq(nil)
+  end
+
+end
