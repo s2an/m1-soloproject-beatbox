3ba9fb289713c7cacf6415cc965ee2d59aed5286
Stephen Nash <stephen.aa.nash@gmail.com>
2023-10-17 18:55:51 -0400

    Flush out with psuedocode

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,25 +1,47 @@
 class LinkedList
 
-  attr_reader :head, :data
+  attr_reader :head #:data, :next_node
 
   def initialize()
     @head = nil
   end
 
-  def append(data)
-    #list.append(string) = data
-    #string = data
-    #data = Node.new(data)
-    #list.append = Node.new(data)
-    #@data = Node.new(data)
-    #list = Node.new(data)
+  def prepend(data)
     new_node = Node.new(data)
+    #new_node.next_node = @head
+    #@head = new_node
     if @head == nil
-      then @head = new_node
-    else @next_node = new_node
-      
+      new_node = @head
     end
   end
+
+  def append(data)
+    new_node = Node.new(data)
+    if @head == nil
+     @head = new_node
+    else
+      current_node = @head
+      while current_node.next_node != nil
+        current_node = current_node.next_node
+      end
+      current_node.next_node = new_node
+    end
+  end
+
+  # incorporate find/search method to find index, then append!
+  # def insert(index, data)
+  #   new_node = Node.new(data)
+  #   if @head == nil
+  #     @head = new_node
+  #   else
+  #     current_node = @head
+  #     while current_node != nil
+  #       current_node = current_node.next_node
+  #     end
+  #     current_node.next_node = new_node
+  #   end
+  # end
+    
 
   def count
     return 0 if head == nil
@@ -33,22 +55,42 @@ class LinkedList
   end
 
   def to_string
-    #@data
-    #@head
-    #p @data
-    #result = @data
-    #for @head
-    head.data.to_s 
+    current = @head
+    result = []
+    while current
+      result << current.data
+      current = current.next_node
+    end
+    result.join(" ")
   end
 
-  def prepend(data)
-    #list.append(string) = data
-    #string = data
-    #data = Node.new(data)
-    #list.append = Node.new(data)
-    #@data = Node.new(data)
-    #list = Node.new(data)
-    @head = Node.new(data)
-  end
+  #to be continued
+
+  # def find(where, how_many)
+  #   nodes.each do |node|
+      
+  # end
+
+  #to be continued
+
+  # def includes?(data)
+  #   nodes.each do |node|
+  #     if node.includes(data)
+  #       return true
+  #     else
+  #       false
+  #     end
+  #   end
+  # end
+
+  #find nil via next_node, move back and index, 
+  #return the node, and then set that node to nil. 
+  #Not the best way, but might work...
+  
+  # def pop
+  #   list.index(next_node, -1)
+  #   return node
+  #   node = nil
+  # end
 
 end
